e81f45fd6592319c287aa7915c990aff45862629
Jordan Lee <netjordanlee.git.mroxo@passmail.net>
2026-01-23 03:14:14 +1100

    feat: implement feature parity with jsonpath-js 0.3.0

--- src/JsonPath.php
+++ src/JsonPath.php
@@ -16,18 +16,38 @@ class JsonPath
 	public function find($json)
 	{
 		$result_node_list = run($json, $this->root_node);
-		return array_map(fn(Node $node) => $node->value, $result_node_list);
+		return array_map(fn (Node $node) => $node->value, $result_node_list);
+	}
+
+	protected function convert_path_segment_to_string($segment)
+	{
+		if (is_string($segment)) {
+			if ($segment === '$') {
+				return $segment;
+			}
+			return "['{$segment}']";
+		}
+		return "[{$segment}]";
 	}
 
 	public function paths($json)
 	{
 		$result_node_list = run($json, $this->root_node);
 		return array_map(
-			fn(Node $node) => [
+			fn (Node $node) => [
 				'value' => $node->value,
-				'path' => $node->path,
+				'path' => implode(array_map(fn ($segment) => $this->convert_path_segment_to_string($segment), $node->path)),
 			],
 			$result_node_list,
 		);
+	}
+
+	public function pathSegments($json)
+	{
+		$result_node_list = run($json, $this->root_node);
+		return array_map(fn (Node $node) => [
+			'value' => $node->value,
+			'segments' => array_slice($node->path, 1)
+		], $result_node_list);
 	}
 }
--- tests/utils/TraverseDescendantTest.php
+++ tests/utils/TraverseDescendantTest.php
@@ -3,25 +3,26 @@
 declare(strict_types=1);
 
 use Loilo\JsonPath\Node;
+
 use function Loilo\JsonPath\create_node;
 use function Loilo\JsonPath\traverse_descendant;
 
 describe('traverseDescendant', function () {
 	test('empty object traverses empty', function () {
-		$node = create_node((object) [], '');
+		$node = create_node((object) [], []);
 		expect(
 			array_map(
-				fn(Node $node) => $node->value,
+				fn (Node $node) => $node->value,
 				traverse_descendant($node),
 			),
 		)->toEqual([(object) []]);
 	});
 
 	test('nested arrays traverse correctly', function () {
-		$node = create_node([[[1]], [2]], '');
+		$node = create_node([[[1]], [2]], []);
 		expect(
 			array_map(
-				fn(Node $node) => $node->value,
+				fn (Node $node) => $node->value,
 				traverse_descendant($node),
 			),
 		)->toEqual([[[[1]], [2]], [[1]], [1], 1, [2], 2]);
